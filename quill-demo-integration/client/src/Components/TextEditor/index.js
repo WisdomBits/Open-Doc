8074f08b5768b57c68692dea641eb057447508b1
Karandeepsingh7070 <soundportrait2530@gmail.com>
2025-01-12 12:20:56 +0530

    moving to client folder

--- quill-demo-integration/client/src/Components/TextEditor/index.js
+++ quill-demo-integration/client/src/Components/TextEditor/index.js
@@ -5,6 +5,23 @@ import "./style.css"
 
 const TextEditor = () => {
 
+    const toolbarOptions = [
+        ['bold', 'italic', 'underline', 'strike'],        // toggled buttons
+        ['blockquote', 'code-block'],
+        ['link', 'image', 'video', 'formula'],
+      
+        [{ 'header': 1 }, { 'header': 2 }],               // custom button values
+        [{ 'list': 'ordered'}, { 'list': 'bullet' }, { 'list': 'check' }],
+        [{ 'script': 'sub'}, { 'script': 'super' }],      // superscript/subscript
+        [{ 'direction': 'rtl' }],                         // text direction
+      
+        [{ 'size': ['small', false, 'large', 'huge'] }],  // custom dropdown
+        [{ 'header': [1, 2, 3, 4, 5, 6, false] }],
+      
+        [{ 'color': [] }, { 'background': [] }],          // dropdown with defaults from theme
+      
+        ['clean']                                         // remove formatting button
+      ];
     // useEffect(() => {
     //     let editor = document.createElement('div')
     //     editorContainerRef.current.append(editor) // to avoid multiple instances of editor
@@ -23,7 +40,10 @@ const TextEditor = () => {
         let editor = document.createElement('div')
         wrapper.append(editor) 
         new Quill(editor, {
-            theme: 'snow'
+            theme: 'snow',
+            modules: {
+                toolbar: toolbarOptions
+              },
         });
     })
   return (<>
