eaf8274777d133a99af1da12a373b75b75c600f6
Karandeepsingh7070 <soundportrait2530@gmail.com>
2025-01-12 13:27:22 +0530

    connecting documents to there own rooms

--- quill-demo-integration/client/src/App.js
+++ quill-demo-integration/client/src/App.js
@@ -1,8 +1,18 @@
 import TextEditor from "./Components/TextEditor";
-
+import {BrowserRouter as Router, Route, Switch, Redirect} from "react-router-dom"
+import { v4 as uuidv4 } from 'uuid';
 function App() {
   return (<>
-  <TextEditor />
+  <Router>
+    <Switch>
+      <Route exact path="/">
+        <Redirect to={`/documents/${uuidv4()}`} />
+      </Route>
+      <Route path="/documents/:id">
+        <TextEditor />
+      </Route>
+    </Switch>
+  </Router>
   </>);
 }
 
--- quill-demo-integration/client/src/Components/TextEditor/index.js
+++ quill-demo-integration/client/src/Components/TextEditor/index.js
@@ -1,53 +1,100 @@
-import React, { useCallback } from 'react'
+import React, { useCallback, useEffect, useState } from 'react'
 import Quill from "quill"
 import "quill/dist/quill.snow.css"
+import {io} from "socket.io-client"
 import "./style.css"
+import { useParams } from 'react-router-dom'
 
+const TOOLBAR_OPTIONS = [
+  [{ header: [1, 2, 3, 4, 5, 6, false] }],
+  [{ font: [] }],
+  [{ list: "ordered" }, { list: "bullet" }],
+  ["bold", "italic", "underline"],
+  [{ color: [] }, { background: [] }],
+  [{ script: "sub" }, { script: "super" }],
+  [{ align: [] }],
+  ["image", "blockquote", "code-block"],
+  ["clean"],
+]
 const TextEditor = () => {
+  const [socket, setSocket] = useState()
+  const [quill, setQuill] = useState()
+  const {id : documentId} = useParams()
+  useEffect(() => {
+    const s = io("http://localhost:3001")
+    setSocket(s)
 
-    const toolbarOptions = [
-        ['bold', 'italic', 'underline', 'strike'],        // toggled buttons
-        ['blockquote', 'code-block'],
-        ['link', 'image', 'video', 'formula'],
-      
-        [{ 'header': 1 }, { 'header': 2 }],               // custom button values
-        [{ 'list': 'ordered'}, { 'list': 'bullet' }, { 'list': 'check' }],
-        [{ 'script': 'sub'}, { 'script': 'super' }],      // superscript/subscript
-        [{ 'direction': 'rtl' }],                         // text direction
-      
-        [{ 'size': ['small', false, 'large', 'huge'] }],  // custom dropdown
-        [{ 'header': [1, 2, 3, 4, 5, 6, false] }],
-      
-        [{ 'color': [] }, { 'background': [] }],          // dropdown with defaults from theme
-      
-        ['clean']                                         // remove formatting button
-      ];
-    // useEffect(() => {
-    //     let editor = document.createElement('div')
-    //     editorContainerRef.current.append(editor) // to avoid multiple instances of editor
-    //     new Quill(editor, {
-    //         theme: 'snow'
-    //       });
-    //       //sddsf
-    //     return () => {
-    //         editorContainerRef.current.innerHTML = ""
-    //     }
-    // },[])
-    // Alternate of above useEffect
-    const editorContainerRef = useCallback((wrapper) => {
-        if (wrapper == null) return
-        wrapper.innerHTML = '' // to avoid multiple instances of editor
-        let editor = document.createElement('div')
-        wrapper.append(editor) 
-        new Quill(editor, {
-            theme: 'snow',
-            modules: {
-                toolbar: toolbarOptions
-              },
-        });
+    return () => {
+      s.disconnect()
+    }
+  }, [])
+
+  useEffect(() => {
+    if (socket == null || quill == null) return
+
+    socket.once("load-document", document => {
+      quill.setContents(document)
+      quill.enable()
     })
+
+    socket.emit("get-document", documentId)
+  }, [socket, quill, documentId])
+
+  // useEffect(() => {
+  //   if (socket == null || quill == null) return
+
+  //   const interval = setInterval(() => {
+  //     socket.emit("save-document", quill.getContents())
+  //   }, SAVE_INTERVAL_MS)
+
+  //   return () => {
+  //     clearInterval(interval)
+  //   }
+  // }, [socket, quill])
+
+  useEffect(() => {
+    if (socket == null || quill == null) return
+
+    const handler = delta => {
+      quill.updateContents(delta)
+    }
+    socket.on("receive-changes", handler)
+
+    return () => {
+      socket.off("receive-changes", handler)
+    }
+  }, [socket, quill])
+
+  useEffect(() => {
+    if (socket == null || quill == null) return
+
+    const handler = (delta, oldDelta, source) => {
+      if (source !== "user") return
+      socket.emit("send-changes", delta)
+    }
+    quill.on("text-change", handler)
+
+    return () => {
+      quill.off("text-change", handler)
+    }
+  }, [socket, quill])
+
+  const wrapperRef = useCallback(wrapper => {
+    if (wrapper == null) return
+
+    wrapper.innerHTML = ""
+    const editor = document.createElement("div")
+    wrapper.append(editor)
+    const q = new Quill(editor, {
+      theme: "snow",
+      modules: { toolbar: TOOLBAR_OPTIONS },
+    })
+    q.disable()
+    q.setText("Loading...")
+    setQuill(q)
+  }, [])
   return (<>
-  <div id='container' ref={editorContainerRef}></div>
+  <div id='container' ref={wrapperRef}></div>
   </>)
 }
 
